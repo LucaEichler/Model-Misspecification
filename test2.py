72d31ba3a19d662fa3c3c57e78638b6684ba0026
LucaE <LucaEichler@gmx.de>
2025-07-15 16:13:34 +0200

    config

--- test2.py
+++ test2.py
@@ -16,6 +16,8 @@ num_iters = 1000000
 model_spec = ('NonLinear', {'dh': dh})
 
 model = in_context_models.InContextModel(dx, dy, 32, 4, 5, model_spec[0], loss, **model_spec[1])
-dataset = datasets.ContextDatasetStream(dataset_amount, dataset_size, model_spec[0], dx, dy, **model_spec[1])
+dataset = datasets.ContextDataset(dataset_amount, dataset_size, model_spec[0], dx, dy, **model_spec[1])
 model_trained = train(model, dataset, iterations=num_iters, batch_size=100,
                   eval_dataset=dataset)
+
+# needs approx. 10k iterations for convergence

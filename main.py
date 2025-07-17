dd8ae315540a6109b6c52cd38995c6b1a58f028c
LucaE <LucaEichler@gmx.de>
2025-07-17 14:13:22 +0200

    plot disable

--- main.py
+++ main.py
@@ -102,13 +102,13 @@ def train(model, dataset, iterations, batch_size, eval_dataset=None, gt_model=No
         tqdm_batch.set_postfix({"loss": loss.item()})
 
         # plotting in case of amortized models
-        if plot:
+        """"if plot:
             if it % 10 == 0 and eval_dataset is not None:
                 eval_data_batch = next(iter(eval_dataloader))  #TODO fix iter
                 model.plot_eval(eval_data_batch, loss_fns)
             # plotting in case of classical models
             elif it % 500 == 0 and gt_model is not None:
-                model.plot_eval(gt_model, loss_fns)
+                model.plot_eval(gt_model, loss_fns)""""
 
     wandb.finish()
 

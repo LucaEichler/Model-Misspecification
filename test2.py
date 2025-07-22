bc2a6e927f6559962e6533798b5effe760f670bb
LucaE <LucaEichler@gmx.de>
2025-07-22 18:02:04 +0200

    conf

--- test2.py
+++ test2.py
@@ -21,7 +21,7 @@ model_spec = ('NonLinear', {'dh': dh})
 model = in_context_models.InContextModel(dx, dy, 256, 4, 5, model_spec[0], loss, **model_spec[1])
 dataset = datasets.ContextDataset(dataset_amount, dataset_size, model_spec[0], dx, dy, **model_spec[1])
 model_trained = train(model, dataset, iterations=num_iters, batch_size=100,
-                  eval_dataset=dataset, lr=0.0001)
+                  eval_dataset=dataset, lr=0.00001)
 
 for i in range(10):
     gt = NonLinear(dx, dy, dh)

26451bd0123809f25c2c4d4735069c5b9d92cd5f
LucaE <LucaEichler@gmx.de>
2025-08-25 17:35:56 +0200

    experiment 3 fix: replace training linear model (wrt the weights) with training nonlinear (neural network) model.

--- experiment3.py
+++ experiment3.py
@@ -6,11 +6,11 @@ import torch
 import config
 import datasets
 import main
-from classical_models import Linear
+from classical_models import Linear, NonLinear
 from main import train, eval_plot
 
 num_iters=50000
-tries = 50
+tries = 1
 sizes = 10  # [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
 test_set_size=10000
 results = torch.zeros(sizes)
@@ -28,19 +28,19 @@ for j in range(tries):
         ds_val = datasets.PointDataset(size=dataset_size, model=gt_model, noise_std=0.5)
 
         model = Linear(dx=dx, dy=dy, order=3, feature_sampling_enabled=False, nonlinear_features_enabled=True)
-        model_trained = train(model, ds, valset=ds_val, valfreq=1000, iterations=num_iters, batch_size=100, lr=config.lr_classical, use_wandb=config.wandb_enabled)
+        model_nn = NonLinear(dx=dx, dy=dy, dh=1000)
+        model_nn = train(model_nn, ds, valset=ds_val, valfreq=1000, iterations=num_iters, batch_size=100, lr=config.lr_classical, use_wandb=config.wandb_enabled)
 
-        params_grad_descent = model_trained.get_W()
         params_mle = model.closed_form_solution_regularized(ds.X.to(config.device), ds.Y.to(config.device), lambd=config.lambda_mle*dataset_size)
 
-        Y_pred = model_trained(test_set.X)
-        Y_pred_mle = model_trained.forward(test_set.X.unsqueeze(0), params_mle.unsqueeze(0))
+        Y_pred_closed_form = model.forward(test_set.X.unsqueeze(0), params_mle.unsqueeze(0))
         gt_Y = gt_model(test_set.X)
 
-        mse_params_closed_form_gradient_descent = torch.mean((params_grad_descent-params_mle)**2)
-        mse_closed_form_gradient_descent = torch.mean((Y_pred-Y_pred_mle)**2)
-        mse_grad_descent = torch.mean((Y_pred-gt_Y)**2)
-        mse_closed_form = torch.mean((Y_pred_mle-gt_Y)**2)
+        Y_pred_nn = model_nn(test_set.X)
+
+        mse_closed_form_gradient_descent = torch.mean((Y_pred_closed_form-Y_pred_nn)**2)
+        mse_grad_descent = torch.mean((Y_pred_nn-gt_Y)**2)
+        mse_closed_form = torch.mean((Y_pred_closed_form-gt_Y)**2)
 
 
         # visualize data through one slice
@@ -55,7 +55,6 @@ for j in range(tries):
         results.append({
             "trial": j,
             "dataset_size": dataset_size,
-            "mse_params_closed_form_gradient_descent": mse_params_closed_form_gradient_descent.item(),
             "mse_closed_form_gradient_descent": mse_closed_form_gradient_descent.item(),
             "mse_grad_descent": mse_grad_descent.item(),
             "mse_closed_form": mse_closed_form.item()

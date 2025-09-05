normalization fix

classical_models.py
@@ -258,7 +258,7 @@ class Linear(nn.Module):
 
         if self.nonlinear_features_enabled:
             # note that the next line also accounts for the normalized case
-            products = torch.pi / 10 * torch.einsum('bi,j->bij', x if scales is None else x*(scales[:,:,1]-scales[:,:,0])+scales[:,:,0], torch.tensor([1,2,3], dtype=x.dtype, device = config.device)).view(batch_size, -1)
+            products = torch.pi / 10 * torch.einsum('bi,j->bij', x if scales is None else x*(scales[:,0:3,1]-scales[:,0:3,0])+scales[:,0:3,0], torch.tensor([1,2,3], dtype=x.dtype, device = config.device)).view(batch_size, -1)
             cos_features = torch.cos(products)
             sin_features = torch.sin(products)
             phi = torch.cat((phi, cos_features), dim=1)
@@ -269,11 +269,11 @@ class Linear(nn.Module):
             centers = torch.stack([a, b, c], dim=-1).view(-1, 3)
             s=torch.ones_like(centers)*4.5
             if scales is not None:  # In case of normalization, we are given scales to recalculate the centers and sizes
-                centers = (centers[None, :, :]-scales[:,:,0])/(scales[:,:, 1]-scales[:,:,0])
-                s = s[None, :, :]/(scales[:,:, 1]-scales[:,:,0])
+                centers = (centers[None, :, :]-scales[:,0:3,0])/(scales[:,0:3, 1]-scales[:,0:3,0])
+                s = s[None, :, :]/(scales[:,0:3, 1]-scales[:,0:3,0])
 
             diff = x[:, None, :] - centers
-            sq_dist = ((diff ** 2)/(2*s**2)).sum(dim=2)
+            sq_dist = ((diff ** 2)/(2*(s**2))).sum(dim=2)
             gauss_features = torch.exp(-sq_dist)
 
             phi = torch.cat((phi, gauss_features), dim=1)

in_context_models.py
@@ -29,10 +29,12 @@ def normalize_input(x):
 
 def normalize_params(params, scales):  # params of shape (batch_size, num_params) #TODO as params are usually sparse, only compute contributions for active terms
     normalized_params = torch.zeros_like(params, dtype=params.dtype)
-    y_min, y_max = scales[:, 0, -1], scales[:, 1, -1]
-    x_min, x_max = scales[:, 0, 0:3], scales[:, 1, 0:3]
+    y_min, y_max = scales[:, -1, 0], scales[:, -1, 1]
+    x_min, x_max = scales[:, 0:3, 0], scales[:, 0:3, 1]
 
-    normalized_params[:, 0] += y_min #TODO check if this correct normalization or we need /ymax-ymin
+    normalized_params[:, 0] += params[:, 0] # add original bias
+
+    normalized_params[:, 0] -= y_min #TODO check if this correct normalization or we need /ymax-ymin
 
     # linear terms
     normalized_params[:, 1:4]+=params[:, 1:4]*(x_max-x_min)
@@ -88,6 +90,9 @@ def normalize_params(params, scales):  # params of shape (batch_size, num_params
         outer_products = outer_products[:, idxs[:, 0], idxs[:, 1], idxs[:, 2]]
 
         normalized_params[:,0]+=torch.sum(params[:, 10:20]*outer_products, dim=-1)
+
+    if params.size(-1) > 20:
+        normalized_params[:,20:65] = params[:,20:65]
 
     normalized_params /= (y_max - y_min).unsqueeze(-1)
 
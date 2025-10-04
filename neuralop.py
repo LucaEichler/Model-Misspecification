59e20fba80c7286a5286cfdb09d72be19ea1a9fb
LucaE <LucaEichler@gmx.de>
2025-10-04 11:48:43 +0200

    neuralop

--- neuralop.py
+++ neuralop.py
@@ -2,13 +2,10 @@ import os
 
 import torch
 
-import config
-import datasets
 from torch import nn
 
 import in_context_models
 import metrics
-from main import train
 
 
 #import debug
@@ -350,61 +347,3 @@ class InterpolationModel(nn.Module):
         out = self(datasets_in[:, :, 0:3], datasets_in[:, :, 0:3], datasets_in[:, :, 3:4], mask)
 
         return metrics.mse(out, y_gt)
-
-model = InterpolationModel()
-print(model)
-default_specs = {
-    'transformer_arch':
-        {
-            'dT': 256,
-            'num_heads': 4,
-            'num_layers': 4,
-            'output': 'attention-pool'
-        },
-    'train_specs':
-        {
-            'lr': 0.0001,
-            'min_lr': 1e-6,
-            'weight_decay': 1e-5,
-            'dataset_amount': 10000,
-            'dataset_size': 128,
-            'num_iters': 1000000,
-            'batch_size': 100,
-            'valset_size': 10000,
-            'normalize': True
-        },
-    'early_stopping_params': {
-        'early_stopping_enabled': False,
-        'patience': 10,
-        'min_delta': 0.01,
-        'load_best': True
-    },
-    #'losses': losses,
-    'save_path': './neural_op_default',
-    'save_all': False
-}
-specs = default_specs
-train_specs = specs['train_specs']
-
-model_specs = [('Linear', {'order': 3, 'feature_sampling_enabled': True}),
-               ('Linear', {'order': 3, 'feature_sampling_enabled': True, 'nonlinear_features_enabled': True}),
-               ('Linear', {'order': 1, 'feature_sampling_enabled': True}), ]
-
-model_spec = model_specs[0]
-x_dist='gaussian'
-noise_std=0.5
-dx, dy = 3,1
-model_path = specs['save_path']
-model_name = 'Neural Operator'
-save_all =specs['save_all']
-os.makedirs(model_path, exist_ok=True)
-dataset = datasets.ContextDataset(train_specs['dataset_amount'], train_specs['dataset_size'], model_spec[0], dx, dy,
-                                  x_dist, noise_std, **model_spec[1])
-valset = datasets.ContextDataset(train_specs['valset_size'], train_specs['dataset_size'], model_spec[0], dx, dy,
-                                 x_dist, noise_std, **model_spec[1])  # TODO valset size in config
-
-model_trained = train(model, dataset, valfreq=500, valset=valset, iterations=train_specs['num_iters'],
-                      batch_size=train_specs['batch_size'],
-                      lr=train_specs['lr'], weight_decay=train_specs['weight_decay'],
-                      early_stopping_params=specs['early_stopping_params'], use_wandb=config.wandb_enabled,
-                      min_lr=train_specs['min_lr'], save_path=model_path, wandb_name=model_name, save_all=save_all)

3adfd95d5dce34aa61cfd7e028029a8e474c1319
LucaE <LucaEichler@gmx.de>
2025-10-16 15:39:37 +0200

    exp2

--- train_neuralop.py
+++ train_neuralop.py
@@ -26,11 +26,11 @@ default_specs = {
             'lr': 0.0001,
             'min_lr': 1e-6,
             'weight_decay': 1e-5,
-            'dataset_amount': 100, #100000,
+            'dataset_amount': 100000,
             'dataset_size': 128,
-            'num_iters': 1, #1000000,
+            'num_iters': 1000000,
             'batch_size': 100,
-            'valset_size': 100, #10000,
+            'valset_size': 10000,
             'normalize': True
         },
     'early_stopping_params': {

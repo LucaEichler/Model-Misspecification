exp2 with fixed scales, no normalization

datasets.py
@@ -10,12 +10,12 @@ from config import device
 from test4 import NonLinearRegression
 
 
-def gen_uniform_bounds(dim, min=-10., max=10., fixed=False):
+def gen_uniform_bounds(dim, min=-10., max=10., x_dist="uniform"):
     bounds = torch.empty((dim,2))
     for i in range(dim):
         lohi = torch.empty(2).uniform_(-10, 10)
         lo, hi = lohi.min(), lohi.max()
-        if fixed:
+        if x_dist == "uniform_fixed":
             lo, hi = min, max
         bounds[i,0], bounds[i,1] = lo, hi
     return bounds
@@ -29,10 +29,11 @@ def sample_dataset(dataset_size, model, x_dist, noise_std=0.0, bounds=None):
     :param bounds: if using uniform mode, specific bounds for each dimension may be provided here
     :return tuple of X and Y of dataset points
     """
-    if x_dist == "uniform":
+    uniform = True if x_dist == "uniform" or "uniform-fixed" else False
+    if uniform:
         X = []
         if bounds is None:
-            bounds = gen_uniform_bounds(model.dx)
+            bounds = gen_uniform_bounds(model.dx, x_dist=x_dist)
         for i in range(model.dx):
             lo, hi = bounds[i, 0], bounds[i, 1]
 

train_neuralop.py
@@ -86,13 +86,13 @@ if __name__ == "__main__":
             for i in range(trials):
                 # create new ground truth model for evaluation
                 gt_model = eval(eval_spec[0])(dx=dx, dy=dy, **eval_spec[1])
-                bounds = datasets.gen_uniform_bounds(dx)
+                bounds = datasets.gen_uniform_bounds(dx, x_dist=x_dist)
 
                 # sample an input dataset from ground truth
-                ds_input = datasets.PointDataset(input_set_size, gt_model, x_dist='uniform', noise_std=0.5, bounds=bounds)
+                ds_input = datasets.PointDataset(input_set_size, gt_model, x_dist=x_dist, noise_std=0.5, bounds=bounds)
 
                 # test dataset, noise disabled to get target function values
-                ds_test = datasets.PointDataset(test_set_size, gt_model, x_dist='uniform', noise_std=0., bounds=bounds)
+                ds_test = datasets.PointDataset(test_set_size, gt_model, x_dist=x_dist, noise_std=0., bounds=bounds)
 
                 predictions = model_trained(ds_test.X.unsqueeze(0), ds_input.X.unsqueeze(0), ds_input.Y.unsqueeze(0), mask=None)
 
@@ -110,7 +110,7 @@ if __name__ == "__main__":
                     Xplot = torch.cat([Xplot, Xplot, Xplot], dim=-1)
                     Yplot = gt_model(Xplot)
 
-                    ds_input_plot = datasets.PointDataset(input_set_size, gt_model, x_dist='uniform', noise_std=0.5,
+                    ds_input_plot = datasets.PointDataset(input_set_size, gt_model, x_dist=x_dist, noise_std=0.5,
                                                           bounds=torch.tensor([[-2., 2.], [-2., 2.], [-2., 2.]]))
 
                     y_pred = model_trained(Xplot.unsqueeze(0), ds_input_plot.X.unsqueeze(0), ds_input_plot.Y.unsqueeze(0), mask=None)
8e29cb58c7eb279478a1234d8f8b06d3e3d7780c
LucaE <LucaEichler@gmx.de>
2025-10-31 16:55:17 +0100

    KL between diag gaussian distributions

--- metrics.py
+++ metrics.py
@@ -17,3 +17,20 @@ def nrmse_range(y_true, y_pred, epsilon=1e-6):
     rmse = torch.sqrt(mse)
     range_y = torch.max(y_true) - torch.min(y_true)
     return rmse / (range_y + epsilon)
+
+
+def KL_diag_gauss(dist1, dist2):
+    """
+    Calculate the KL divergence between two diagonal gaussian distributions
+    """
+    means1, vars1 = dist1
+    means2, vars2 = dist2
+
+    KL_div = 0.5 * torch.sum(
+        (vars1 / vars2)
+        + ((means2 - means1) ** 2) / vars2
+        - 1
+        + torch.log(vars2 / vars1),
+        dim=-1  # sum over feature dimension
+    )
+    return KL_div
